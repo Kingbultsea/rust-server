eb9293dae5be52fbe08b89ab8336620aa722762d
譜 <894275157@qq.com>
2021-09-04 13:10:53 +0800

    mod

---
+++ restaurant/src/front_of_house.rs
@@ -0,0 +1,23 @@
+// pub mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {
+//             println!("add_to_waitlist")
+//         }
+//     }
+//
+//     mod serving {
+//         fn take_order() {
+//             serve_order();
+//         }
+//
+//         fn serve_order() {
+//
+//         }
+//
+//         fn fntake_payment() {
+//
+//         }
+//     }
+// }
+
+pub mod hosting;
